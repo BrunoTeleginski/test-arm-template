209eff0ac4846b998fd1a43b336d6aae66424b53
Bruno Teleginski <brunovini2009@gmail.com>
2021-08-30 20:31:01 +0000

    added vnet and commands reads output

---
+++ reads_output.ps1
@@ -0,0 +1,39 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string]$ArmOutputString,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [bool]$lowerCase
+)
+
+Write-Output "Retrieved input: $ArmOutputString"
+$armOutputObj = $ArmOutputString | ConvertFrom-Json
+
+$armOutputObj.PSObject.Properties | ForEach-Object {
+    $type = ($_.value.type).ToLower()
+    $keyname = $_.Name
+    $vsoAttribs = @("task.setvariable variable=$keyName")
+
+    if ($type -eq "array") {
+        $value = $_.Value.value.name -join ',' ## All array variables will come out as comma-separated strings
+    } elseif ($type -eq "securestring") {
+        $vsoAttribs += 'isSecret=true'
+    } elseif ($type -ne "string") {
+        throw "Type '$type' is not supported for '$keyname'"
+    } else {
+        $value = $_.Value.value
+    }
+
+    $attribString = $vsoAttribs -join ';'
+
+    if($lowerCase){
+        $value = $($value).ToLower()
+    }
+
+    $var = "##vso[$attribString]$value"
+    
+    Write-Output -InputObject $var
+}
